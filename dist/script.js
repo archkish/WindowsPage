8212e9d31c32596b5fd4c84773a1bb4ff3b8dcec
Artur Radchenko <kisharchi@gmail.com>
2025-07-23 23:16:13 +0200

    add some fix

--- dist/script.js
+++ dist/script.js
@@ -211,7 +211,8 @@ const modals = state => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll('[data-modal]');
+      windows = document.querySelectorAll('[data-modal]'),
+      scroll = calcScroll();
     trigger.forEach(item => {
       item.addEventListener('click', e => {
         if (e.target) {
@@ -235,6 +236,7 @@ const modals = state => {
         });
         modal.style.display = 'block';
         document.body.style.overflow = 'hiden';
+        document.body.style.marginRight = `${scroll}px`;
         // document.body.classList.add('modal-open')
       });
     });
@@ -244,6 +246,8 @@ const modals = state => {
       });
       modal.style.display = 'none';
       document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
+
       // document.body.classList.remove('modal-open')
     });
     modal.addEventListener('click', e => {
@@ -253,6 +257,7 @@ const modals = state => {
         });
         modal.style.display = 'none';
         document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
         // document.body.classList.remove('modal-open')
       }
     });
@@ -265,6 +270,17 @@ const modals = state => {
     }, time);
   }
   ;
+  function calcScroll() {
+    let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+  }
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
